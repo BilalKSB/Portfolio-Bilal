031d6503e8f346cbda954df92c069f3b226b112f
Bilal <boukhorissa.bilal@gmail.com>
2024-07-08 18:17:05 +0200

    essai prefix images error

--- next.config.js
+++ next.config.js
@@ -5,8 +5,9 @@ const nextConfig = {
   output: 'export',
   distDir: 'out',
   images: {
-   unoptimized: true,
+    unoptimized: true,
   },
-}
+  assetPrefix: './', // Ajoutez cette ligne pour s'assurer que les chemins des actifs sont correctement résolus
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
